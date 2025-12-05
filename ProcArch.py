f84e7f3a89846c4d055a6d99be94c69e0e91ebff
JoseOliden <158219397+JoseOliden@users.noreply.github.com>
2025-12-05 15:06:02 -0500

    Update ProcArch.py

--- ProcArch.py
+++ ProcArch.py
@@ -85,7 +85,7 @@ def Selecion_Nucleidos_Au(df_rpt_Au,df_Nucleidos, df_database):
     En_Au = float(411.8) 
     tol_Au = float(1.0)
     df_rpt_Au["Energy (keV)"] = pd.to_numeric(df_rpt_Au["Energy (keV)"], errors="coerce")
-    df_energy_Au = df_rpt_Au[(df_rpt_Au["Tentative Nuclide"] == "AU-198") & ((df_rpt_Au["Energy (keV)"] > En_Au + tol_Au) | (df_rpt_Au["Energy (keV)"] < En_Au - tol_Au))]
+    df_energy_Au = df_rpt_Au[(df_rpt_Au["Tentative Nuclide"] == "AU-198") & ((df_rpt_Au["Energy (keV)"] > En_Au - tol_Au) | (df_rpt_Au["Energy (keV)"] < En_Au + tol_Au))]
     return df_energy_Au   
 def Selecion_Nucleidos(df_rpt_Au,df_Nucleidos, df_databas):
     df_filtrado = df[(df[""] == "P") & ((df["Energía"] > 100) | (df["Energía"] < 20))]
