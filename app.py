652e0dea1130f7d471f670e0182d19b0924d6d9d
JoseOliden <158219397+JoseOliden@users.noreply.github.com>
2025-12-05 14:48:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -230,7 +230,7 @@ elif page == "📊 Procesamiento":
             st.session_state['resultados'] = df_ejemplo
             st.session_state['procesado'] = True
 
-            df_Au = Selecion_Nucleidos_Au(df_au_resultado,ref_files, df_file)
+            df_Au = Selecion_Nucleidos_Au(st.session_state.df_au_resultado,ref_files, df_file)
             st.dataframe(df_Au)
 
 # ============================================
