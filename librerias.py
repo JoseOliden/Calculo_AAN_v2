8a9c25d935dfd438a7b19af9fbb153e44fa2ad85
JoseOliden <158219397+JoseOliden@users.noreply.github.com>
2025-12-05 14:02:31 -0500

    Update librerias.py

--- librerias.py
+++ librerias.py
@@ -1,1 +1,14 @@
-
+import streamlit as st
+import pandas as pd
+import numpy as np
+import openpyxl
+import os
+import re
+import io
+import json
+from datetime import datetime
+from scipy.optimize import root
+import sympy as sp
+import matplotlib.pyplot as plt
+import tempfile
+import base64
