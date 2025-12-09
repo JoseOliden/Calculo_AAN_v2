431bbbea80c96b1e2f202159b9f0086ab6c25982
JoseOliden <158219397+JoseOliden@users.noreply.github.com>
2025-12-08 20:30:16 -0500

    Update calAAN.py

--- calAAN.py
+++ calAAN.py
@@ -104,7 +104,7 @@ def conc(df_muestra, w,td_i,ti_i,tv_i,tr_i, df_comp_Au, w_Au,td_c_Au,ti_c_Au,tv_
   if geometria == 185:
     e_c_Au = df_comp_Au["EFIGAMMA185"].to_numpy()*df_comp_Au["COI GAMMA185"].to_numpy()
   Q0_c_Au = df_comp_Au["Q0"].to_numpy()
-  Cn_c_Au = df_comp_Au["NET Peak Area"].to_numpy()
+  Cn_c_Au = df_comp_Au["Net Peak Area"].to_numpy()
   w_c_Au = w_Au
   lam_c_Au = log(2)/df_comp_Au["t(1/2) s"].to_numpy()
   Er_c_Au = df_comp_Au["EREF"].to_numpy()
@@ -121,7 +121,7 @@ def conc(df_muestra, w,td_i,ti_i,tv_i,tr_i, df_comp_Au, w_Au,td_c_Au,ti_c_Au,tv_
   if geometria == 185:
     e_i = df_muestra["EFIGAMMA185"].to_numpy()*df_muestra["COI GAMMA185"].to_numpy()
   Q0_i = df_muestra["Q0"].to_numpy()
-  Cn_i = df_muestra["NET Peak Area"].to_numpy()
+  Cn_i = df_muestra["Net Peak Area"].to_numpy()
   w_i = w
   lam_i = log(2)/df_muestra["t(1/2) s"].to_numpy()
   Er_i = df_muestra["EREF"].to_numpy()
