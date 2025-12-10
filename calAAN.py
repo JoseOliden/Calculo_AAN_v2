bdd16da4a4c880c3861eaa232e726903e941b8b0
JoseOliden <158219397+JoseOliden@users.noreply.github.com>
2025-12-10 11:21:08 -0500

    Update calAAN.py

--- calAAN.py
+++ calAAN.py
@@ -93,8 +93,8 @@ def cal_Q0_alfa_i(Q0,Er,alfa):
   return (Q0-0.429)/(Er**alfa) + 0.429/(2*alfa+0.55**alfa)
 
 def conc(df_muestra, w,td_i,ti_i,tv_i,tr_i, df_comp_Au, w_Au,td_c_Au,ti_c_Au,tv_c_Au,tr_c_Au, alfa, f, geometria):
-  alfa = 0.226 # Forzar valor de alfa 
-  f = 34       # Forzar valor de f
+  #alfa = 0.226 # Forzar valor de alfa 
+  #f = 34       # Forzar valor de f
   st.dataframe(df_comp_Au)
   # Comparador Au
   #k0_c_Au, e_c_Au, Q0_c_Au, Cn_c_Au, w_c_Au, lam_c_Au, Er_c_Au, td_c_Au, tr_c_Au, ti_c_Au, tv_c_Au =  par_comp_Au
