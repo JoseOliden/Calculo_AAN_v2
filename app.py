6c51519da97c2a895fdec8f2d0e6b64bfa32ca73
JoseOliden <158219397+JoseOliden@users.noreply.github.com>
2025-12-14 17:49:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ st.markdown("""
 
 st.image(
     "banner.jpg",
-    width=100,
+    width=1000,
 )
 # Título principal
 st.markdown('<h1 class="main-header">Sistema de Análisis AAN</h1>', unsafe_allow_html=True)
