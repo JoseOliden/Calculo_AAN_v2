6c3367d0c56bada2ab08db40926ec8caa3b30ae1
JoseOliden <158219397+JoseOliden@users.noreply.github.com>
2025-12-17 08:14:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -277,9 +277,10 @@ elif page == "📊 Procesamiento":
                         st.session_state["df_comparadores_alfa_f"]
             
                     # Procesa comparador de Au y sus datos
-                    st.dataframe(df_Au)
+                    
 
                     df_Au = Selecion_Nucleidos_Au(st.session_state["df_au_resultado"], st.session_state["df_file"],st.session_state["tolerancia"])
+                    st.dataframe(df_Au)
                     # Hallar los nucleidos y sus datos
                     df_filtrado_Nuclidos = Selecion_Nucleidos_muestra(st.session_state["df_resultado"],st.session_state["ref_files"], st.session_state["df_file"], st.session_state["tolerancia"])
 
